c649dfb4f4ee14ebc9e0d954c542265d28b4010a
Lio Bouchan <liogothicvampire@gmail.com>
2018-07-13 22:42:12 -0500

    update and show images of albums

--- controllers/album.js
+++ controllers/album.js
@@ -115,11 +115,54 @@ function deleteAlbum(req, res) {
 	});
 }
 
+function uploadImage(req, res){
+	var albumId = req.params.id;
+	var file_name = 'No subido...';
+
+	if(req.files){
+		var file_path = req.files.image.path;
+		var file_split = file_path.split('\\');
+		var file_name = file_split[2];
+		
+		var ext_split = file_name.split('\.');
+		var file_ext = ext_split[1];
+
+		if (file_ext == 'png' || file_ext == 'jpg' || file_ext == 'gif'){
+			Album.findByIdAndUpdate(albumId, {image: file_name}, (err, albumUpdated) =>{
+				if (!albumUpdated) {
+					res.status(404).send({
+						message: 'No se ha podido actualizar el usuario'
+					});	
+				}else{
+					res.status(200).send({album: albumUpdated});
+				}
+			});
+		}else{
+			res.status(200).send({message: 'Extensión del archivo no valida.'});
+		}
+	}else{
+		res.status(200).send({message: 'No has subido ninguna imagen...'});
+	}
+}
+
+function getImageFile(req, res){
+	var imageFile = req.params.imageFile;
+	var path_file = './uploads/albums/'+imageFile;
+	fs.exists(path_file , function(exists){
+		if (exists) {
+			res.sendFile(path.resolve(path_file));
+		}else{
+			res.status(200).send({message: 'No existe la imagen...'});
+		}
+	})
+}
 
 module.exports= {
 	getAlbum,
 	saveAlbum,
 	getAlbums,
 	updateAlbum,
-	deleteAlbum
+	deleteAlbum,
+	uploadImage,
+	getImageFile
 };
